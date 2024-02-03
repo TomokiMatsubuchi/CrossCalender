6f4645bd8be21dc658ce70dfaf2c72ba9c6520e5
tomoki matsubuchi <matsubuchitmk@gmail.com>
2024-02-04 00:41:52 +0900

    axiosのimport 修正

--- next/app/api/auth/[...nextauth]/route.ts
+++ next/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
-import { AxiosError } from 'axios'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import NextAuth, { User as NextAuthUser, Session } from 'next-auth'
 import { JWT } from 'next-auth/jwt'
 import CredentialsProvider from 'next-auth/providers/credentials'
