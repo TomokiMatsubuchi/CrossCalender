182e8034238212567fb262839679d2de1b69a1c3
tomoki matsubuchi <matsubuchitmk@gmail.com>
2024-03-15 23:15:05 +0900

    build出来なくなってしまったので、next.configを修正

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -1,13 +1,11 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    config.resolve.alias = {
-      '@': path.resolve(__dirname, './app/'),
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 export default nextConfig
